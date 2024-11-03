2fea294de66074b2ec016a86b5ead9f6351a82c8
M3hTi <mehdinasir502@yahoo.com>
2024-11-03 15:39:49 +0330

    add placeBet method to pkerGame object

--- objects.js
+++ objects.js
@@ -2,5 +2,9 @@
 const pokerGame = {
     currentBank: null,
     currentBet: null,
+    placeBet: function(){
+        this.currentBank -= this.currentBet;
+        return  this.currentBank;
+    }
 }
 
